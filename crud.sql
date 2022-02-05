b7bdff6d897d4828358e7d5e4c3a71782f67cb31
Nyande Paul <nyande20@gmail.com>
2022-02-06 02:55:03 +0530

    Add files via upload

---
+++ crud.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 31, 2022 at 06:05 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `sid` int(10) NOT NULL,
+  `sname` varchar(30) NOT NULL,
+  `saddress` varchar(100) NOT NULL,
+  `sclass` int(10) NOT NULL,
+  `sphone` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`sid`, `sname`, `saddress`, `sclass`, `sphone`) VALUES
+(16, 'Darshan Bhat', 'Alkapuri', 2, '9873828382'),
+(17, 'Hannah Rajput', 'Alkapuri', 2, '9867452325'),
+(18, 'Nyande Paul Natale', 'Pratapgunj', 2, '9782162231');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentclass`
+--
+
+CREATE TABLE `studentclass` (
+  `cid` int(11) NOT NULL,
+  `cname` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `studentclass`
+--
+
+INSERT INTO `studentclass` (`cid`, `cname`) VALUES
+(1, 'BCA'),
+(2, 'MSc IT'),
+(3, 'PHD');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `studentclass`
+--
+ALTER TABLE `studentclass`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `sid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `studentclass`
+--
+ALTER TABLE `studentclass`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
